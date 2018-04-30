0d1599feac108046c9e4d31c9b84f76403b661e9
olane <oli@gearset.com>
2018-04-30 13:30:37 +0100

    Make content script better 👉👌

--- src/content_script.js
+++ src/content_script.js
@@ -1,4 +1,8 @@
+const emojiRegex = require('emoji-regex');
+
 walk(document.body);
+
+setInterval(() => walk(document.body), 15000);
 
 function walk(node) 
 {
@@ -7,8 +11,8 @@ function walk(node)
 	
 	var child, next;
 	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
+	if ((node.tagName && (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'))
+	    || (node.classList && node.classList.contains('ace_editor'))) {
 		return;
 	}
 
@@ -32,14 +36,17 @@ function walk(node)
 	}
 }
 
+
 function handleText(textNode) 
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	const rude = ['🍆', '🖕', '👉👌', '💩'];
+
+	var regex = emojiRegex();
+	var item = rude[Math.floor(Math.random()*rude.length)];
+
+	v = v.replace(regex, item);
 	
 	textNode.nodeValue = v;
 }
